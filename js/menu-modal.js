a872751ef3ec2c258f9366207069a02ddd7fb8f4
Viktor Zolotukhin <zolotukhinvm@ya.ru>
2023-03-15 12:05:01 +0300

    027 add ajax & modal for form

--- js/menu-modal.js
+++ js/menu-modal.js
@@ -1,12 +1,12 @@
 let burger  = document.querySelector('.hamburger');
-let overlay = document.querySelector('.overlay');
+let overlay = document.querySelector('.overlay-menu');
 let links = document.querySelectorAll('.menu--vertical .menu__link');
 let body = document.body;
 
 function toggleMenu(burgerLink){
   burgerLink.preventDefault();
   burger.classList.toggle('hamburger--active');
-  overlay.classList.toggle('overlay--active');
+  overlay.classList.toggle('overlay-menu--active');
   body.classList.toggle('body--active-menu');
 }
 
---
+++ js/slider.js
@@ -0,0 +1,7 @@
+$('.catalog__list').slick({
+  prevArrow: $('.catalog__arrow--prev'),
+  nextArrow: $('.catalog__arrow--next'),
+  speed: 500,
+  slidesToShow: 1,
+  // centerMode: true
+});
